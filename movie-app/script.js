c28cd2a2bf39e7c26b191cd66301f76bb82a3440
amarza <adrianoctavmarza@gmail.com>
2021-09-25 10:43:40 +0200

    finished movie app and add background slider folder

--- movie-app/script.js
+++ movie-app/script.js
@@ -5,6 +5,7 @@ const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=c554561d31
 const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
+
 // Get initial movies
 
 getMovies(API_URL)
@@ -13,23 +14,19 @@ async function getMovies(url) {
     const res = await fetch(url)
     const data = await res.json()
 
-    console.log(data.results)
+    showMovies(data.results)
 }
 
 function showMovies(movies) {
     main.innerHTML = ''
-
-    main.appendChild(movieEl)
     
     movies.forEach((movie) => {
-        const {title, poster_path, vote_average, overview} = movie
+        const { title, poster_path, vote_average, overview } = movie
         
         const movieEl = document.createElement('div')
         movieEl.classList.add('movie')
         
-        movieEl.innerHTML = 
-        `
-        <div class="movie">
+        movieEl.innerHTML = `
             <img src="${IMG_PATH + poster_path}" alt="${title}">
             <div class="movie-info">
                 <h3>${title}</h3>
@@ -39,8 +36,8 @@ function showMovies(movies) {
                 <h3>Overview</h3>
                 ${overview}
             </div>
-        </div>
         `
+        main.appendChild(movieEl)
     })
 }
 
